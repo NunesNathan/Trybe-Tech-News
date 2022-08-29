fad2bf991c0cb0c67a56d50e50c0916bf7fc5280
NunesNathan <nunesxd7@hotmail.com>
2022-08-29 15:01:28 -0300

    feat: scrape noticia works

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -15,21 +15,30 @@ def fetch(url):
         return response.text
 
 
-# Requisito 2
 def scrape_novidades(html_content):
     selector = Selector(html_content)
     return selector.css("a.cs-overlay-link::attr(href)").getall()
 
 
-# Requisito 3
 def scrape_next_page_link(html_content):
     selector = Selector(html_content)
     return selector.css(".next::attr(href)").get()
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css("h1.entry-title::text").get().strip(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css(".author a::text").get(),
+        "comments_count": len(selector.css("#comments").getall()),
+        "summary": "".join(
+            selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+        ).strip(),
+        "tags": selector.css(".post-tags a::text").getall(),
+        "category": selector.css("div.meta-category .label::text").get()
+    }
 
 
 # Requisito 5
